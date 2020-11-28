bd31a89eaa9307d2ee09d8312a287d6db83ab414
turentoo <69717562+turentoo@users.noreply.github.com>
2020-11-28 10:26:07 +0000

    Initial Commit

---
+++ guessFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guessFlag
+//
+//  Created by Alice Nedosekina on 28/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
